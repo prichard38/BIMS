d3b70262d54e2a2766611a1ea4f4bfc014173a6c
shoemaker30 <72107198+shoemaker30@users.noreply.github.com>
2021-10-05 12:44:05 -0400

    Create Procedure_LogIn.sql

---
+++ SQL/Procedure_LogIn.sql
@@ -0,0 +1,19 @@
+/* Stored procedure takes input username and password. */
+/* Returns role of user or NULL if the user is not in the database */
+
+CREATE PROCEDURE LogIn(IN user_id VARCHAR(45), IN user_password VARCHAR(45), OUT user_role VARCHAR(45))
+BEGIN
+
+	DECLARE role_num INT;
+    	DECLARE role_name VARCHAR(45);
+
+	SELECT UserRole INTO role_num
+	FROM Users
+	WHERE UserId = user_id AND UserPassword=user_password;
+
+	SELECT UserRoleName INTO role_name
+	FROM UserRole
+	WHERE UserRole = role_num;
+    
+    	SET user_role = role_name;
+END
