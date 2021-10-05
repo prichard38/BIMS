1b76aa6f86ae4f73bcc039d6ac21909d37007091
shoemaker30 <72107198+shoemaker30@users.noreply.github.com>
2021-10-05 16:22:58 -0400

    Add files via upload

---
+++ dbConfig.php
@@ -0,0 +1,20 @@
+<?php
+
+//database information
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "mydb";
+
+
+// Create connection
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    header('Location: connect-error.html');
+    exit;
+}
+?>
+
