7e976a2b53e157dc7f0a9a35b5459bd371345064
GooseBumps23 <robinson336@marshall.edu>
2021-10-25 13:03:05 -0400

    10 year selecction range

--- supervisor_longitudinal_analysis.php
+++ supervisor_longitudinal_analysis.php
@@ -115,6 +115,11 @@
                         <option value="2019">2019</option>
                         <option value="2018">2018</option>
                         <option value="2017">2017</option>
+                        <option value="2016">2016</option>
+                        <option value="2015">2015</option>
+                        <option value="2014">2014</option>
+                        <option value="2013">2013</option>
+                        <option value="2012">2012</option>
                     </select>
                     Year:
                     <select name="year" id="year_selector" required>
@@ -123,6 +128,11 @@
                         <option value="2019">2019</option>
                         <option value="2018">2018</option>
                         <option value="2017">2017</option>
+                        <option value="2016">2016</option>
+                        <option value="2015">2015</option>
+                        <option value="2014">2014</option>
+                        <option value="2013">2013</option>
+                        <option value="2012">2012</option>
                     </select>
                 </p>
             </div>  
