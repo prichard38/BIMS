e19361a1388e8b97e28a9d3ce80bb354790e3ef4
shoemaker30 <72107198+shoemaker30@users.noreply.github.com>
2021-10-26 17:36:24 -0400

    Update Procedure_LA_Tool.sql

--- SQL/Procedure_LA_Tool.sql
+++ SQL/Procedure_LA_Tool.sql
@@ -35,6 +35,25 @@ END$$
 
 DELIMITER;
 
+DELIMITER$$
+
+CREATE PROCEDURE selectBridgeInspectionData_BetweenYears(IN bridge_name VARCHAR(45), IN begin_year int, IN end_year int)
+BEGIN
+	SELECT i.FinishedDate, i.Bridges_BridgeNo, b.BridgeName, t.InspectionTypeName ,u.FirstName AS inspector_first, 
+	u.LastName AS inspector_last, u2.FirstName AS evaluator_first, u2.LastName AS evaluator_last, i.OverallRating
+	FROM Inspections i 
+	JOIN Bridges b ON i.Bridges_BridgeNo = b.BridgeNo
+	JOIN InspectionTypeCode t ON i.InspectionTypeNo = t.InspectionTypeNo
+	JOIN Users u ON i.InspectorID = u.UserNo
+	JOIN Users u2 ON i.EvaluatorID = u2.UserNo
+	WHERE Bridges_BridgeNo IN (
+		SELECT BridgeNo FROM Bridges WHERE BridgeName=bridge_name
+	) 
+    AND YEAR(DATE(FinishedDate)) >= begin_year
+    AND YEAR(DATE(FinishedDate)) <= end_year;
+END$$
+
+DELIMITER;
 
 
 
