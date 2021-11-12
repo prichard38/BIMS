3ff19990be369f1df26125aba6cc0fb68e4b0987
shoemaker30 <shoemaker30@marshall.edu>
2021-11-11 21:25:14 -0500

    added stored procedure for api

---
+++ SQL/Procedure_REST_API.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+/** This procedure grabs information about an inspection for the mobile application to use. 
+ *  The app uses an inspection id and needs this info.
+*/
+CREATE PROCEDURE selectInspectionData_ById(IN inspection_id int)
+BEGIN
+    SELECT i.InspectionID, i.Bridges_BridgeNo, b.BridgeName, i.AssignedDate, i.DueDate, i.FinishedDate, 
+    t.InspectionTypeName,  i.OverallRating, u.FirstName AS inspector_first, 
+    u.LastName AS inspector_last, u2.FirstName AS evaluator_first, u2.LastName AS evaluator_last
+    FROM Inspections i 
+    JOIN Bridges b ON i.Bridges_BridgeNo = b.BridgeNo
+    JOIN InspectionTypeCode t ON i.InspectionTypeNo = t.InspectionTypeNo
+    JOIN Users u ON i.InspectorID = u.UserNo
+    JOIN Users u2 ON i.EvaluatorID = u2.UserNo
+    WHERE InspectionID = inspection_id;
+END$$
+
+DELIMITER;
