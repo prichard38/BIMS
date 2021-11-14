699964ef94d76b45912dc4d89eda34c1bd7a1939
shoemaker30 <72107198+shoemaker30@users.noreply.github.com>
2021-11-14 18:55:27 -0500

    fixed indenting and put working query first

--- SQL/Procedure_YIR_Tool.sql
+++ SQL/Procedure_YIR_Tool.sql
@@ -1,27 +1,8 @@
-
-
-/*
-Old unfinished query. 
-Worked fast to get newest report for each bridge in one given year.
-However, I could not get joins to work, so it was replaces with the second, slower method for now. 
-*/
-/*
-CREATE PROCEDURE selectNewestInspectionData_ByYear(IN inspec_year int)
-BEGIN
-	SELECT Inspections.* FROM Inspections, 
-	(SELECT Bridges_BridgeNo, MAX(FinishedDate) AS newest_date FROM Inspections WHERE year(FinishedDate)=inspec_year GROUP BY Bridges_BridgeNo) newest_reports
-	WHERE Inspections.Bridges_BridgeNo=newest_reports.Bridges_BridgeNo
-	AND Inspections.FinishedDate=newest_reports.newest_date;
-END$$
-*/
-
-
-
 DELIMITER$$
 
 CREATE PROCEDURE selectNewestInspectionData_ByYear(IN inspec_year int)
 BEGIN
-SELECT *
+	SELECT *
 	FROM (
 		SELECT i.FinishedDate, i.DueDate, i.Bridges_BridgeNo, b.BridgeName, t.InspectionTypeName ,u.FirstName AS inspector_first, 
 		u.LastName AS inspector_last, u2.FirstName AS evaluator_first, u2.LastName AS evaluator_last, i.OverallRating
@@ -41,3 +22,20 @@ SELECT *
 END$$
 
 DELIMITER;
+
+/*
+Old unfinished query. 
+Worked fast to get newest report for each bridge in one given year.
+However, I could not get joins to work, so it was replaces with the second, slower method for now. 
+*/
+/*
+CREATE PROCEDURE selectNewestInspectionData_ByYear(IN inspec_year int)
+BEGIN
+	SELECT Inspections.* FROM Inspections, 
+	(SELECT Bridges_BridgeNo, MAX(FinishedDate) AS newest_date FROM Inspections WHERE year(FinishedDate)=inspec_year GROUP BY Bridges_BridgeNo) newest_reports
+	WHERE Inspections.Bridges_BridgeNo=newest_reports.Bridges_BridgeNo
+	AND Inspections.FinishedDate=newest_reports.newest_date;
+END$$
+*/
+
+
