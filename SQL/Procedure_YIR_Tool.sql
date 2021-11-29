7f5edf05512c3aa3511fc3d31cce22855a4b9793
shoemaker30 <shoemaker30@marshall.edu>
2021-11-29 09:18:07 -0500

    Final versi

--- SQL/Procedure_YIR_Tool.sql
+++ SQL/Procedure_YIR_Tool.sql
@@ -1,3 +1,6 @@
+/* Current best query but didn't take non-started inspections into account
+	Also need to grab the Status row */
+/*
 DELIMITER $$
 
 CREATE PROCEDURE selectNewestInspectionData_ByYear(IN inspec_year int)
@@ -19,6 +22,32 @@ BEGIN
 		WHERE year(FinishedDate)= inspec_year 
 		GROUP BY Bridges_BridgeNo
 	);
+END$$
+
+DELIMITER;
+*/
+
+/*
+	Temporary Query for demo today 11-29 
+	works fine but need to ensure the group by grabs the newest inspection for final project
+*/
+DELIMITER $$
+
+CREATE PROCEDURE selectNewestInspectionData_ByYear(IN inspec_year int)
+BEGIN
+SELECT *
+	FROM (
+		SELECT i.FinishedDate, i.DueDate, i.Bridges_BridgeNo, b.BridgeName, i.Status, t.InspectionTypeName ,u.FirstName AS inspector_first, 
+		u.LastName AS inspector_last, u2.FirstName AS evaluator_first, u2.LastName AS evaluator_last, i.OverallRating
+		FROM Inspections i 
+		JOIN Bridges b ON i.Bridges_BridgeNo = b.BridgeNo
+		JOIN InspectionTypeCode t ON i.InspectionTypeNo = t.InspectionTypeNo
+		JOIN Users u ON i.InspectorID = u.UserNo
+		JOIN Users u2 ON i.EvaluatorID = u2.UserNo
+		WHERE YEAR(DATE(FinishedDate)) = inspec_year
+        OR YEAR(DATE(DueDate)) = inspec_year
+	) AS inspection_data
+    GROUP BY Bridges_BridgeNo;
 END$$
 
 DELIMITER;
